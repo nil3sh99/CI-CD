6422e618542d0a2fb9cbff2ea62c3ccf014460c0
Nilesh Aggarwal <nilesh.aggarwal@bell.ca>
2024-07-31 01:17:34 -0600

    testing github actions workflow

--- aws-ci-cd/lambda/main.py
+++ aws-ci-cd/lambda/main.py
@@ -1,8 +1,9 @@
-
-
 def handler(event, context):
-    print(event)
+    response_body = {
+        "message": "Hello World",
+        "version": "1.0.0"
+    }
     return {
         "statusCode" : 200, 
-        "body": "Hello World"
+        "body": response_body
     }
